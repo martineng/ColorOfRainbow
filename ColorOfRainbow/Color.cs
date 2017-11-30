4b40a8028b50c5fedff592ff2d56aa7c4d56cbe6
martineng <martin.eca.6651@gmail.com>
2017-11-30 23:40:52 +0800

    Initial git with Color class, Newtonsoft.json added

---
+++ ColorOfRainbow/Color.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColorOfRainbow
+{
+    class Color
+    {
+        public string name { get; set; }
+        public string hexadecimal { get; set; }
+    }
+}
